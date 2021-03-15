818512e7c2534c861ed312af1b1ec6cb4e2e377c
Poonnanun <poonnanun.p@ku.th>
2021-03-15 10:12:36 +0700

    Add concurency

--- hello.go
+++ hello.go
@@ -6,8 +6,11 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"strings"
 )
+
+var wg sync.WaitGroup
 
 type SitemapIndex struct {
 	Locations []string `xml:"sitemap>loc"`
@@ -33,27 +36,43 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, GO is here</h1>")
 }
 
+func newsRoutine(c chan News, Location string){
+    defer wg.Done()
+    var n News
+
+    Location = strings.TrimSpace(Location)
+    resp, _ := http.Get(Location)
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    xml.Unmarshal(bytes, &n)
+    resp.Body.Close()
+    c <- n
+}
+
 func newAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s SitemapIndex
-	var n News
-	news_map := make(map[string]NewsMap)
 
-	res, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(res.Body)
-	xml.Unmarshal(bytes, &s)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    xml.Unmarshal(bytes, &s)
+    news_map := make(map[string]NewsMap)
+    resp.Body.Close()
+    queue := make(chan News, 30)
 
-	for _, Location := range s.Locations {
-		Location = strings.TrimSpace(Location)
-		res, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(res.Body)
-		xml.Unmarshal(bytes, &n)
+    for _, Location := range s.Locations {
+        wg.Add(1)
+        go newsRoutine(queue, Location)
+    }
+    wg.Wait()
+    close(queue)
 
-		for idx, _ := range n.Keywords {
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-	}
+    for elem := range queue {
+        for idx, _ := range elem.Keywords {
+            news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
+        }
+    }
 
-	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+    p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+
 	t, _ := template.ParseFiles("newsTemplate.html")
 	t.Execute(w, p)
 }
